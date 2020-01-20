75a9bb60fe8a0abf26a7020d563bcb0db4405b9c
iamsomraj <iamsomraj@gmail.com>
2020-01-21 03:11:59 +0530

    Updates

---
+++ precache-manifest.f7a67154db7cdf3850eafe0477abd611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0930f1c5a79ca81142ba7d1de121b7",
+    "url": "/jsx/index.html"
+  },
+  {
+    "revision": "81e2058abeb92e28e2c0",
+    "url": "/jsx/static/js/2.06b5ce91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jsx/static/js/2.06b5ce91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0170bbcd2788b8b2ef9b",
+    "url": "/jsx/static/js/main.59f4a872.chunk.js"
+  },
+  {
+    "revision": "7942c7d85001e40aaecc",
+    "url": "/jsx/static/js/runtime-main.3efb43e2.js"
+  }
+]);
